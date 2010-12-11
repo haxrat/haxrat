3670fcdfb6b69979e81d950c678b8cd49eb9a817
haxrat <haxrat@gmail.com>
2010-12-12 07:48:49 +0800

    change | separator to - in title

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 	 */
 	global $page, $paged;
 
-	wp_title( '|', true, 'right' );
+	wp_title( '-', true, 'right' );
 
 	// Add the blog name.
 	bloginfo( 'name' );
@@ -23,11 +23,11 @@
 	// Add the blog description for the home/front page.
 	$site_description = get_bloginfo( 'description', 'display' );
 	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
+		echo " - $site_description";
 
 	// Add a page number if necessary:
 	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'toolbox' ), max( $paged, $page ) );
+		echo ' - ' . sprintf( __( '第%s页', 'haxrat' ), max( $paged, $page ) );
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
