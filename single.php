fd670708d7ef8520dcb42593aecdeb135ddc5871
haxrat <haxrat@gmail.com>
2010-12-20 02:34:59 +0800

    remove top navigation in single.php

--- single.php
+++ single.php
@@ -11,12 +11,6 @@ get_header(); ?>
 
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-				<nav id="nav-above">
-					<h1 class="section-heading"><?php _e( 'Post navigation', 'toolbox' ); ?></h1>
-					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'toolbox' ) . '</span> %title' ); ?></div>
-					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'toolbox' ) . '</span>' ); ?></div>
-				</nav><!-- #nav-above -->
-
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header">
 						<h1 class="entry-title"><?php the_title(); ?></h1>
