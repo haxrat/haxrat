6faf5cab0f3dc8065a057c7175dda43d0cf4c429
haxrat <haxrat@gmail.com>
2010-12-20 01:16:05 +0800

    change to basic pagination

--- loop.php
+++ loop.php
@@ -50,11 +50,7 @@
 
 <?php endwhile; ?>
 
-<?php /* Display navigation to next/previous pages when applicable */ ?>
-<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-	<nav id="nav-below">
-		<h1 class="section-heading"><?php _e( 'Post navigation', 'toolbox' ); ?></h1>
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'toolbox' ) ); ?></div>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'toolbox' ) ); ?></div>
-	</nav><!-- #nav-below -->
-<?php endif; ?>
+<?php if (function_exists("pagination")) { 
+	pagination($additional_loop->max_num_pages);
+} ?>
+
