db43823dddd36b46f5c6e563fd27b048fe49111b
haxrat <haxrat@gmail.com>
2010-12-20 18:55:05 +0800

    change permalink text to chinese

--- loop.php
+++ loop.php
@@ -8,7 +8,7 @@
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
-			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( '点击阅读全文', 'haxrat' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
 			<div class="entry-meta">
 				<?php
