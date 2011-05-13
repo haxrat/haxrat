0cbd7373d2dc86f46214ffa9cf121645877f90bd
haxrat <haxrat@gmail.com>
2011-05-13 17:45:18 +0800

    fix loading local cdn

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 </div><!-- #page -->
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.1.min.js">\x3C/script>')</script>
+<script>window.jQuery || document.write("<script src='<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.6.1.min.js'>\x3C/script>")</script>
 <script src="<?php bloginfo( 'template_directory' ); ?>/js/script.js"></script>
 <?php wp_footer(); ?>
 
